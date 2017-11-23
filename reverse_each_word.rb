1d17ae03332496bb1a393443ce9c545a969549c9
Cat Norris <cannenorris@yahoo.com>
2017-11-23 23:34:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,2 @@
 sentence="Hello there, and how are you?"
 
-def reverse_each_word (sentence)
-  sentence=sentence.split
-  newwords=[]
-sentence.each do |word|
-  newword=word.reverse
-   newwords=<<"#{newword} "
- end
- newwords
- end
